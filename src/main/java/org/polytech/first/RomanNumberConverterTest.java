8879b46690d2a68af8171a04717a39070f352656
sargisEg <sargis.eg@gmail.com>
2023-10-01 22:32:47 +0400

    Roman to Integer problem

---
+++ src/main/java/org/polytech/first/RomanNumberConverterTest.java
@@ -0,0 +1,13 @@
+package main.java.org.polytech.first;
+
+public class RomanNumberConverterTest {
+
+    public static void main(String[] args) {
+        RomanNumberConverter romanNumberConverter = new RomanNumberConverter();
+        System.out.println(romanNumberConverter.convertToInt("MMMCMXCIX"));
+        System.out.println(romanNumberConverter.convertToInt("III"));
+        System.out.println(romanNumberConverter.convertToInt("LVIII"));
+        System.out.println(romanNumberConverter.convertToInt("MCMXCIV"));
+        System.out.println(romanNumberConverter.convertToInt("MDLX"));
+    }
+}
